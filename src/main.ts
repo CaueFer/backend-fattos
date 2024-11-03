de73f22db919478e57398852717b1b0cf82be0a7
cauefer <cauefernandescaetano@gmail.com>
2024-11-03 15:30:41 -0300

    FIX: cors dnv

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,38 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle("Backent Fattos")
-    .setDescription("API Fattos Technical Challeng, connected with PostgreSQL")
-    .setVersion("1.0")
+    .setTitle('Backent Fattos')
+    .setDescription('API Fattos Technical Challeng, connected with PostgreSQL')
+    .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // remove propriedades não definidas no DTO
-    forbidNonWhitelisted: true, // proíbe propriedades não definidas no DTO
-    transform: true
-}));
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://frontend-fattos.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove propriedades não definidas no DTO
+      forbidNonWhitelisted: true, // proíbe propriedades não definidas no DTO
+      transform: true,
+    }),
+  );
 
   const port = process.env.DB_PORT || 5000;
 
   await app.listen(port, () => {
-    console.log("Server Nest On: ", port);
+    console.log('Server Nest On: ', port);
   });
 }
 bootstrap();
